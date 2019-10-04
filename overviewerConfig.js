eed394725c08cbd889dcb95fe03c9b92d8310011
gaspard <gaspard@gmail.com>
2019-10-04 15:09:59 +0200

    disable overlays

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -25,7 +25,7 @@ var overviewerConfig = {
     "worlds": [
         "overworld_2019-09-30",
         "overworld_2019-09-03",
-        "minecraft - nether"
+        // "minecraft - nether"
     ],
     "tilesets": [
         {
@@ -80,7 +80,7 @@ var overviewerConfig = {
                 89,
                 176
             ],
-            "path": "2019-09-30_nigh"
+            "path": "2019-09-30_night"
         },
         {
             "north_direction": 0,
@@ -96,7 +96,7 @@ var overviewerConfig = {
                 80,
                 176
             ],
-            "name": "Night",
+            "name": "Day",
             "base": "https://flexas.s3.amazonaws.com/",
             "defaultZoom": 1,
             "bgcolor": "#1a1a1a",
@@ -136,125 +136,29 @@ var overviewerConfig = {
             ],
             "path": "2019-09-03_night"
         },
-        {
-            "north_direction": 0,
-            "world": "minecraft - nether",
-            "poititle": "Markers",
-            "imgextension": "png",
-            "maxZoom": 7,
-            "last_rendertime": 1567376740,
-            "minZoom": 0,
-            "zoomLevels": 7,
-            "spawn": false,
-            "name": "Nether",
-            "base": "",
-            "defaultZoom": 1,
-            "bgcolor": "#1a1a1a",
-            "isOverlay": false,
-            "showlocationmarker": true,
-            "center": [
-                -128,
-                80,
-                176
-            ],
-            "path": "nether"
-        },
-        {
-            "north_direction": 0,
-            "world": "overworld_2019-09-03",
-            "poititle": "Markers",
-            "imgextension": "png",
-            "maxZoom": 10,
-            "last_rendertime": 1567444452,
-            "minZoom": 0,
-            "zoomLevels": 10,
-            "spawn": [
-                -128,
-                80,
-                176
-            ],
-            "name": "Biome Coloring Overlay",
-            "base": "",
-            "defaultZoom": 1,
-            "tilesets": [
-                "day"
-            ],
-            "bgcolor": "#1a1a1a",
-            "isOverlay": [
-                "day"
-            ],
-            "showlocationmarker": true,
-            "center": [
-                -128,
-                80,
-                176
-            ],
-            "path": "overlay_biome"
-        },
-        {
-            "north_direction": 0,
-            "world": "overworld_2019-09-03",
-            "poititle": "Markers",
-            "imgextension": "png",
-            "maxZoom": 10,
-            "last_rendertime": 1567444452,
-            "minZoom": 0,
-            "zoomLevels": 10,
-            "spawn": [
-                -128,
-                80,
-                176
-            ],
-            "name": "Mob Spawnable Areas Overlay",
-            "base": "",
-            "defaultZoom": 1,
-            "tilesets": [
-                "day"
-            ],
-            "bgcolor": "#1a1a1a",
-            "isOverlay": [
-                "day"
-            ],
-            "showlocationmarker": true,
-            "center": [
-                -128,
-                80,
-                176
-            ],
-            "path": "overlay_mobs"
-        },
-        {
-            "north_direction": 0,
-            "world": "overworld_2019-09-03",
-            "poititle": "Markers",
-            "imgextension": "png",
-            "maxZoom": 10,
-            "last_rendertime": 1567444452,
-            "minZoom": 0,
-            "zoomLevels": 10,
-            "spawn": [
-                -128,
-                80,
-                176
-            ],
-            "name": "Slime Chunk Overlay",
-            "base": "",
-            "defaultZoom": 1,
-            "tilesets": [
-                "day"
-            ],
-            "bgcolor": "#1a1a1a",
-            "isOverlay": [
-                "day"
-            ],
-            "showlocationmarker": true,
-            "center": [
-                -128,
-                80,
-                176
-            ],
-            "path": "overlay_slime"
-        }
+        // {
+        //     "north_direction": 0,
+        //     "world": "minecraft - nether",
+        //     "poititle": "Markers",
+        //     "imgextension": "png",
+        //     "maxZoom": 7,
+        //     "last_rendertime": 1567376740,
+        //     "minZoom": 0,
+        //     "zoomLevels": 7,
+        //     "spawn": false,
+        //     "name": "Nether",
+        //     "base": "",
+        //     "defaultZoom": 1,
+        //     "bgcolor": "#1a1a1a",
+        //     "isOverlay": false,
+        //     "showlocationmarker": true,
+        //     "center": [
+        //         -128,
+        //         80,
+        //         176
+        //     ],
+        //     "path": "nether"
+        // },
     ],
     "map": {
         "north_direction": "lower-left",
